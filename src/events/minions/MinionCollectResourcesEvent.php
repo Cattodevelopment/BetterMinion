5b306164c2cd08c6dcd87d181177d50990753152
Khang Nguyen <mcbeany7@gmail.com>
2022-02-28 08:43:02 +0700

    Wtf git

---
+++ src/events/minions/MinionCollectResourcesEvent.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mcbeany\BetterMinion\events\minions;
+
+use Mcbeany\BetterMinion\minions\entities\BaseMinion;
+use pocketmine\event\Cancellable;
+use pocketmine\event\CancellableTrait;
+use pocketmine\item\Item;
+
+class MinionCollectResourcesEvent extends MinionEvent implements Cancellable{
+	use CancellableTrait;
+
+	/**
+	 * @param Item[] $drops
+	 */
+	public function __construct(
+		BaseMinion $minion,
+		private array $drops
+	) {
+		parent::__construct($minion);
+	}
+
+	public function getDrops() : Item{
+		return $this->drops;
+	}
+
+	/**
+	 * @param Item[] $drops
+	 */
+	public function setDrops(array $drops) : void{
+		$this->drops = $drops;
+	}
+}
