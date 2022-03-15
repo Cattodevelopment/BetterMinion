b92ce43cdff8818ee77f4a04a57c61bd0ef219a1
Mcbeany <mcbeany7@gmail.com>
2022-03-15 16:53:54 +0700

    Add constants

--- src/minion/information/MinionInformation.php
+++ src/minion/information/MinionInformation.php
@@ -7,10 +7,14 @@ namespace Mcbeany\BetterMinion\minion\information;
 use pocketmine\nbt\tag\CompoundTag;
 
 class MinionInformation implements MinionNBT {
+	public const MIN_LEVEL = 1;
+	public const MAX_LEVEL = 15;
+	//TODO: Add lock feature
+	
 	public function __construct(
 		private MinionType $type,
 		private MinionUpgrade $upgrade,
-		private int $level
+		private int $level = self::MIN_LEVEL
 	) {
 	}
 
