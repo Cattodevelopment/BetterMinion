69824c9270e5b11678625d97e60e19830c162523
RealCatto <102582305+RealCatto@users.noreply.github.com>
2022-04-25 13:42:22 +0530

    Add files via upload

--- src/Mcbeany/BetterMinion/command/argument/TypeArgument.php
+++ src/Mcbeany/BetterMinion/command/argument/TypeArgument.php
@@ -2,26 +2,29 @@
 
 declare(strict_types=1);
 
-namespace Mcbeany\BetterMinion\command\argument;
+namespace Mcbeany\BetterMinion\commands\arguments;
 
 use CortexPE\Commando\args\StringEnumArgument;
-use Mcbeany\BetterMinion\minion\MinionFactory;
+use Mcbeany\BetterMinion\minions\MinionType;
 use pocketmine\command\CommandSender;
-use function array_keys;
+use function array_map;
+use function array_values;
+use function strtolower;
 
 class TypeArgument extends StringEnumArgument{
+
+	public function __construct(){ parent::__construct("type", true); }
+
 	public function parse(string $argument, CommandSender $sender) : string{
-		return $argument;
+		return strtolower($argument);
 	}
 
 	public function getTypeName() : string{
 		return "string";
 	}
 
-	/**
-	 * @return array<string>
-	 */
 	public function getEnumValues() : array{
-		return array_keys(MinionFactory::getInstance()->getDefaultTypes());
+		return array_values(array_map(fn(MinionType $type) => $type->name(), MinionType::getAll()));
 	}
+
 }
